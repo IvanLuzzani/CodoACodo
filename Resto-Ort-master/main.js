852b4c9b6ce1a59699059e256683b9b4762ed406
Ivan <Ivan.luzzani@gmail.com>
2022-09-22 19:40:45 -0300

    Dandole formato al codigo

--- Resto-Ort-master/main.js
+++ Resto-Ort-master/main.js
@@ -1,9 +1,9 @@
 
-let mail = document.getElementById("mail"), 
-    telefono = document.getElementById("telefono"), 
-    nombre = document.getElementById("nombre"), 
+let mail = document.getElementById("mail"),
+    telefono = document.getElementById("telefono"),
+    nombre = document.getElementById("nombre"),
     apellido = document.getElementById("apellido"),
-    fecha = document.getElementById("fecha"), 
+    fecha = document.getElementById("fecha"),
     hora = document.getElementById("hora"),
     enviar = document.getElementById("enviar"),
     form = document.getElementById("form"),
@@ -11,28 +11,28 @@ let mail = document.getElementById("mail"),
 
 function validarMail() {
     const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if(!regex.test(mail.value)){
-        alert("Email inválido");  
-    }else{
+    if (!regex.test(mail.value)) {
+        alert("Email inválido");
+    } else {
         return true;
     }
-    
+
 }
 
-function validarTelefono(){
-   if(isNaN(telefono.value)){
-       alert("Telefono inválido");
+function validarTelefono() {
+    if (isNaN(telefono.value)) {
+        alert("Telefono inválido");
 
-   }else{
-       return true;
-   }
+    } else {
+        return true;
+    }
 }
 
-function validarVacio(){
+function validarVacio() {
     let i = 0;
     let inputValue = true;
-    while(i < inputs.length && inputValue == true){
-        if(inputs[i].value.trim() == ""){
+    while (i < inputs.length && inputValue == true) {
+        if (inputs[i].value.trim() == "") {
             inputValue = alert("Los campos no pueden estar vacíos")
         }
         i++;
@@ -41,42 +41,42 @@ function validarVacio(){
 }
 
 // Función para cambiar el formato de fecha del form.
-    function formato(texto){
-        return texto.replace(/^(\d{4})-(\d{2})-(\d{2})$/g,'$3/$2/$1');
-  }
+function formato(texto) {
+    return texto.replace(/^(\d{4})-(\d{2})-(\d{2})$/g, '$3/$2/$1');
+}
 
-function validarDia(){
+function validarDia() {
     let f = new Date();
     let dia = `0${f.getDate()}`.slice(-2),
         mes = `0${f.getMonth() + 1}`.slice(-2),
         anio = f.getFullYear();
-        
+
     let fechaActual = dia + "-" + mes + "-" + anio;
 
-    if(formato(fecha.value) < (dia + mes + anio)){
+    if (formato(fecha.value) < (dia + mes + anio)) {
         alert(`La fecha debe ser: ${fechaActual} o mayor`);
- 
-    }else{
+
+    } else {
         return true;
     }
 }
 
-function validarHora(){
+function validarHora() {
     console.log(hora.value);
-    if(hora.value < "10:00" || hora.value > "23:00"){
+    if (hora.value < "10:00" || hora.value > "23:00") {
         alert("La hora debe estar entre las: 10:00 y las 23:00");
- 
-    }else{
+
+    } else {
         return true;
     }
 }
-    
-form.addEventListener("submit", function(e){
-    if(!validarVacio() || !validarMail() || !validarTelefono() || !validarDia() || !validarHora()){
+
+form.addEventListener("submit", function (e) {
+    if (!validarVacio() || !validarMail() || !validarTelefono() || !validarDia() || !validarHora()) {
         e.preventDefault();
     }
 
-} )
+})
 
 
 
